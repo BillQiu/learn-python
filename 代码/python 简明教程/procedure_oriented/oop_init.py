8a46d63186e0ebda541edd9e8a30405a1dd32f24
Bill <fanglaiq@gmail.com>
2017-09-28 16:50:12 +0800

    feat: oop

---
+++ 代码/python 简明教程/procedure_oriented/oop_init.py
@@ -0,0 +1,8 @@
+class Person():
+    def __init__(self, name):
+        self.name = name
+    def sayName(self):
+        print(self.name)
+
+p = Person('Bill')
+p.sayName()
---
+++ 代码/python 简明教程/procedure_oriented/oop_method.py
@@ -0,0 +1,6 @@
+class Person:
+    def say_hi(self):
+        print('hellp')
+
+p = Person()
+p.say_hi()
